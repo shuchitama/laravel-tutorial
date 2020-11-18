096f1ddcd7ed61dada50399a36c13dfd90d304e2
Shuchita Mahey <shuchita@live.com>
2020-11-18 15:55:45 -0800

    CRUD operations with raw SQL

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return 'I am in User COntroller';
+        // DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['Shuchita', 'shuchita@live.com', 'password']);
+        // $users = DB::select('select * from users');
+
+        // DB::update('update users set name = ? where id = 1', ["Amita"]);
+        // $users = DB::select('select * from users');
+
+        // DB::delete('delete from users where id=1');
+        // $users = DB::select('select * from users');
+        // return $users;
+        return view('home');
     }
 }

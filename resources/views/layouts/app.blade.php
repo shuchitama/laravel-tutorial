495a927ab9227afe2bcc01a5d457d1ce22f55d7d
Shuchita Mahey <shuchita@live.com>
2020-11-20 14:25:31 -0800

    update authenticated user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,11 +56,13 @@
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false" v-pre>
                 {{ Auth::user()->name }}
+                <img src="{{ Auth::user()->avatar }}" alt="avatar" />
               </a>
+
 
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                  document.getElementById('logout-form').submit();">
                   {{ __('Logout') }}
                 </a>
 

bb1ae4a3b2ea875c19bb85068c49b57c90ee0477
Shuchita Mahey <shuchita@live.com>
2020-11-25 14:32:58 -0800

    build out create todo view using tailwinds for CSS

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,1 +1,22 @@
-Create new todo
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Todos</title>
+</head>
+
+<body>
+  <div class="text-center pt-10">
+    <h1 class="text-2xl">What next you need To-DO</h1>
+    <form method="post" action="/todo/create" class="py-5 border">
+      @csrf
+      <input type="text" name="title" class="py-2 px-2 border rounded" />
+      <input type="submit" value="Create" class="p-2 border rounded" />
+    </form>
+  </div>
+</body>
+
+</html>

072f9644eba1dafef573353118e813257280a9a6
Shuchita Mahey <shuchita@live.com>
2020-11-26 11:53:30 -0800

    style todo list

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,8 +1,11 @@
 @extends('todos.layout')
 
 @section('content')
-<h1 class="text-2xl">All your Todos</h1>
-<ul>
+<div class="flex justify-center">
+  <h1 class="text-2xl">All your Todos</h1>
+  <a href="todos/create" class=" mx-5 py-1 px-1 bg-blue-400 cursor-pointer rounded text-white">Create new</a>
+</div>
+<ul class="my-5">
   @foreach($todos as $todo)
   <li>
     {{ $todo->title }}

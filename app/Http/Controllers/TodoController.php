fe612f57c5a8a0b94ff0039f90791a992fa2e58c
Shuchita Mahey <shuchita@live.com>
2020-11-26 14:42:38 -0800

    completed items show at bottom of list

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -11,7 +11,7 @@ class TodoController extends Controller
 {
     public function index()
     {
-       $todos = Todo::all();
+       $todos = Todo::orderBy('completed')->get();
        return view('todos.index', compact('todos'));
     }
 

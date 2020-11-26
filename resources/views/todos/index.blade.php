ec87a5d4d3e47d25ade399e926ed403b7320ee97
Shuchita Mahey <shuchita@live.com>
2020-11-26 14:02:42 -0800

    validate on edit/update

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -5,8 +5,8 @@
   <h1 class="text-2xl">All your Todos</h1>
   <a href="todos/create" class="mx-5 py-1 px-1 bg-blue-400 cursor-pointer rounded text-white">Create new</a>
 </div>
-<x-alert />
 <ul class="my-5">
+  <x-alert />
   @foreach($todos as $todo)
   <li class="flex justify-center py-2">
     <p>{{ $todo->title }}</p>

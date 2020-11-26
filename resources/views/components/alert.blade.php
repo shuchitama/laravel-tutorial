8412a9e514b7eb5d5a2df93d149e20155396c4a1
Shuchita Mahey <shuchita@live.com>
2020-11-25 16:47:14 -0800

    add background color to alert messages

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,9 @@
 <div>
   @if(session()->has('message'))
-  <div class="alert alert-success">{{session()->get('message')}}</div>
+  {{ $slot }}
+  <div class="py-4 px-2 bg-green-300">{{session()->get('message')}}</div>
   @elseif(session()->has('error'))
-  <div class="alert alert-danger">{{session()->get('error')}}</div>
+  {{ $slot }}
+  <div class="py-4 px-2 bg-red-300">{{session()->get('error')}}</div>
   @endif
 </div>

7f6c1107c1773389dcb675ff2879034b0cf1cf6a
Shuchita Mahey <shuchita@live.com>
2020-11-26 11:25:09 -0800

    style todo list

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,8 +1,24 @@
-All todos
-<ul>
-  @foreach($todos as $todo)
-  <li>
-    {{ $todo->title }}
-  </li>
-  @endforeach
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Todos</title>
+</head>
+
+<body>
+  <div class="text-center pt-10">
+    <h1 class="text-2xl">All your Todos</h1>
+    <ul>
+      @foreach($todos as $todo)
+      <li>
+        {{ $todo->title }}
+      </li>
+      @endforeach
+    </ul>
+  </div>
+</body>
+
+</html>

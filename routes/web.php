458e5b3ebd7367ec46f6265a6ae3fd19d40e1a66
Shuchita Mahey <shuchita@live.com>
2020-11-30 09:40:29 -0800

    make resource controller

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,16 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/todos', 'TodoController@index')->name('todo.index');
-Route::get('/todos/create', 'TodoController@create');
-Route::post('/todos/create', 'TodoController@store');
-Route::get('/todos/{todo}/edit', 'TodoController@edit');
-Route::patch('/todos/{todo}/update', 'TodoController@update')->name('todo.update');
+Route::resource('/todos', 'TodoController');
+// Route::get('/todos', 'TodoController@index')->name('todo.index');
+// Route::get('/todos/create', 'TodoController@create');
+// Route::post('/todos/create', 'TodoController@store');
+// Route::get('/todos/{todo}/edit', 'TodoController@edit');
+// Route::patch('/todos/{todo}/update', 'TodoController@update')->name('todo.update');
+// Route::delete('/todos/{todo}/delete', 'TodoController@delete')->name('todo.delete');
+
 Route::put('/todos/{todo}/complete', 'TodoController@complete')->name('todo.complete');
 Route::put('/todos/{todo}/incomplete', 'TodoController@incomplete')->name('todo.incomplete');
-Route::delete('/todos/{todo}/delete', 'TodoController@delete')->name('todo.delete');
 
 Route::get('/', function () {
     return view('welcome');
